545d9e453e23a2ed893d3b2b01a5f0a031eb72a9
minglong51 <56749246+minglong51@users.noreply.github.com>
2020-11-30 00:41:41 -0600

    Update TwitchClient.java

--- src/main/java/com/laioffer/jupiter/external/TwitchClient.java
+++ src/main/java/com/laioffer/jupiter/external/TwitchClient.java
@@ -20,6 +20,8 @@ import java.util.List;
 
 
 public class TwitchClient {
+    
+    #use token and client id for authenication
     private static final String TOKEN = "Bearer pj5svw4bfwppglu57rv11g5c9z2awy";
     private static final String CLIENT_ID = "uu88rd6bo4o4dx4pdhthovfdlij79o";
     private static final String TOP_GAME_URL_TEMPLATE = "https://api.twitch.tv/helix/games/top?first=%s";
@@ -28,6 +30,8 @@ public class TwitchClient {
 
     private String searchTwitch(String url){
         CloseableHttpClient httpclient = HttpClients.createDefault();
+        
+        //response handler check null or error from response and return valid response body
         ResponseHandler<String> responseHandler = response ->{
             int responseCode = response.getStatusLine().getStatusCode();
             if (responseCode != 200) {
@@ -42,10 +46,12 @@ public class TwitchClient {
             JSONObject obj = new JSONObject(EntityUtils.toString(entity));
             return obj.getJSONArray("data").toString();
         };
+        //try catch block executing request and return response body
         try {
             HttpGet request = new HttpGet(url);
             request.setHeader("Authorization", TOKEN);
             request.setHeader("Client-Id", CLIENT_ID);
+            //return response body in string after checked
             return httpclient.execute(request, responseHandler);
         } catch (IOException e) {
             e.printStackTrace();
@@ -58,7 +64,8 @@ public class TwitchClient {
             }
         }
     }
-
+    
+    //ObjectMapper convert data to Game[] array
     private List<Game> getGameList(String data) throws TwitchException {
         ObjectMapper mapper = new ObjectMapper();
         try {
@@ -68,14 +75,16 @@ public class TwitchClient {
             throw new TwitchException("Failed to parse game data from Twitch API");
         }
     }
-
+        
+    //search topGames with limit 0 is default
     public List<Game> topGames(int limit) throws TwitchException {
         if (limit <= 0) {
             limit = DEFAULT_GAME_LIMIT;
         }
         return getGameList(searchTwitch(buildGameURL(TOP_GAME_URL_TEMPLATE, "", limit)));
     }
-
+    
+    //search specific game from twitch
     public Game searchGame(String gameName) throws TwitchException {
         List<Game> gameList = getGameList(searchTwitch(buildGameURL(GAME_SEARCH_URL_TEMPLATE, gameName, 0)));
         if (gameList.size() != 0) {
@@ -83,12 +92,14 @@ public class TwitchClient {
         }
         return null;
     }
-
+    
+    //building url function
     private String buildGameURL(String url, String gameName, int limit) {
         if (gameName.equals("")) {
             return String.format(url, limit);
         } else {
             try {
+                //encode game name, e.g. convert space to %20
                 gameName = URLEncoder.encode(gameName, "UTF-8");
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
