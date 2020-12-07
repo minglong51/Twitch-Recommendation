744604f650761158cac0075d3781643f9df290da
minglong51 <minglongpan51@gmail.com>
2020-12-06 18:52:41 -0600

    add favorite function

--- src/main/java/com/laioffer/jupiter/db/MySQLTableCreator.java
+++ src/main/java/com/laioffer/jupiter/db/MySQLTableCreator.java
@@ -64,7 +64,10 @@ public class MySQLTableCreator {
                     + "FOREIGN KEY (item_id) REFERENCES items(id)"
                     + ")";
             statement.executeUpdate(sql);
+
             // Step 4: insert fake user 1111/3229c1097c00d497a0fd282d586be050.
+            sql = "INSERT INTO users VALUES('1111', '3229c1097c00d497a0fd282d586be050', 'John', 'Smith')";
+            statement.executeUpdate(sql);
 
             conn.close();
             System.out.println("Import done successfully");
---
+++ src/main/java/com/laioffer/jupiter/entity/FavoriteRequestBody.java
@@ -0,0 +1,22 @@
+package com.laioffer.jupiter.entity;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class FavoriteRequestBody {
+    private final Item favoriteItem;
+
+    //new a favorite request body
+    //JsonCreator (for constructor) has similar meaning as JsonDeserialize(for builder pattern)
+    //use @JsonProperty only convert json(favorite) to Item
+    //if use it above favorite Item, we can still convert it back from object to jason
+    @JsonCreator
+    public FavoriteRequestBody(@JsonProperty("favorite") Item favoriteItem) {
+        this.favoriteItem = favoriteItem;
+    }
+
+    public Item getFavoriteItem() {
+        return favoriteItem;
+    }
+}
+
